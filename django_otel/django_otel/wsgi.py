3d64684bf031d2408c55dce4a1b116249ba7fa00
veziak <veziak@yahoo.com>
2024-08-15 20:34:28 +0100

    add opentelemetry

--- django_otel/django_otel/wsgi.py
+++ django_otel/django_otel/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_otel.settings")
 
 application = get_wsgi_application()
+application = OpenTelemetryMiddleware(application)
--- django_otel/manage.py
+++ django_otel/manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from opentelemetry.instrumentation.django import DjangoInstrumentor
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_otel.settings")
+    DjangoInstrumentor().instrument()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
